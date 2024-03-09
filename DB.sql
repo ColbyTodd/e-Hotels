573b4e479431e9173cd01f22c5560ac83ca34c5e
Colby Todd <cjhtodd11@gmail.com>
2024-03-09 17:36:33 -0500

    Added hotel_chain, hotel and room table

---
+++ DB.sql
@@ -0,0 +1,57 @@
+-- ----------------------------
+-- Table structure for hotel_chain
+-- ----------------------------
+DROP TABLE IF EXISTS hotel_chain;
+CREATE TABLE hotel_chain (
+  id serial PRIMARY KEY,
+  number_of_hotels int NOT NULL CHECK (number_of_hotels > 0),
+  address_of_central_offices varchar(100) NOT NULL,
+  email_addresses varchar(100)[] NOT NULL,
+  phone_numbers varchar(10)[] NOT NULL
+);
+
+-- ----------------------------
+-- Records of hotel_chain
+-- ----------------------------
+INSERT INTO hotel_chain VALUES (1, 8, '22 Prince Street', ARRAY['hotel@hotel.com', 'guestservices@hotel.com'], ARRAY['5555555555', '7777777777']);
+
+-- ----------------------------
+-- Table structure for hotel
+-- ----------------------------
+DROP TABLE IF EXISTS hotel;
+CREATE TABLE IF NOT EXISTS hotel (
+  id serial PRIMARY KEY,
+  hotel_chain_id int NOT NULL REFERENCES hotel_chain(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  category int NOT NULL CHECK (category > 0 and category <= 5),
+  number_of_rooms int NOT NULL CHECK (number_of_rooms > 0),
+  address varchar(100) NOT NULL,
+  email varchar(100) NOT NULL CHECK (email LIKE '_%@_%._%'),
+  phone varchar(10) NOT NULL
+);
+
+-- ----------------------------
+-- Records of hotel
+-- ----------------------------
+INSERT INTO hotel VALUES (1, 1, 3, 102, '49 Terrence Avenue', 'inn@inn.com', '1231231234');
+
+-- ----------------------------
+-- Table structure for room
+-- ----------------------------
+DROP TABLE IF EXISTS room;
+CREATE TABLE room (
+    id serial PRIMARY KEY,
+    hotel_id int NOT NULL REFERENCES hotel(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    hotel_chain_id int NOT NULL REFERENCES hotel_chain(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    price int NOT NULL CHECK (price > 0),
+    amenities varchar(100),
+    capacity int NOT NULL CHECK (capacity > 0),
+    room_view varchar(8) CHECK (room_view = 'sea' OR room_view = 'mountain'),
+    extendable bit NOT NULL,
+    problems bit NOT NULL,
+    room_status bit NOT NULL
+);
+
+-- ----------------------------
+-- Records of room
+-- ----------------------------
+INSERT INTO room VALUES (1, 1, 1, 59, , 2, 'sea', 1, 0, 0);
