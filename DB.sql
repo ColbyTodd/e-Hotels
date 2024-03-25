1a32931e4550e682fe38ea7c1dc86b17f3d3c22e
Colby Todd <cjhtodd11@gmail.com>
2024-03-25 18:36:29 -0400

    Added hotel_chain_email sample data

--- DB.sql
+++ DB.sql
@@ -32,8 +32,31 @@ CREATE TABLE hotel_chain_email (
 -- ----------------------------
 -- Records of hotel_chain_email
 -- ----------------------------
-INSERT INTO hotel_chain_email VALUES (1, 1, 'hotel@hotel.com');
-INSERT INTO hotel_chain_email VALUES (2, 1, 'guestservices@hotel.com');
+insert into hotel_chain_email (id, hotel_chain_id, email) values (1, 1, 'vjinkin0@biblegateway.com');
+insert into hotel_chain_email (id, hotel_chain_id, email) values (2, 4, 'mpartner1@gizmodo.com');
+insert into hotel_chain_email (id, hotel_chain_id, email) values (3, 3, 'bgrealey2@mlb.com');
+insert into hotel_chain_email (id, hotel_chain_id, email) values (4, 2, 'qbehrendsen3@goo.gl');
+insert into hotel_chain_email (id, hotel_chain_id, email) values (5, 5, 'jkorneluk4@cbc.ca');
+insert into hotel_chain_email (id, hotel_chain_id, email) values (6, 2, 'sphilippault5@homestead.com');
+insert into hotel_chain_email (id, hotel_chain_id, email) values (7, 1, 'ggreenhow6@bizjournals.com');
+insert into hotel_chain_email (id, hotel_chain_id, email) values (8, 4, 'bclayfield7@cam.ac.uk');
+insert into hotel_chain_email (id, hotel_chain_id, email) values (9, 3, 'abevans8@bloglovin.com');
+insert into hotel_chain_email (id, hotel_chain_id, email) values (10, 3, 'nwardrope9@telegraph.co.uk');
+insert into hotel_chain_email (id, hotel_chain_id, email) values (11, 5, 'cwardingtona@github.io');
+insert into hotel_chain_email (id, hotel_chain_id, email) values (12, 2, 'jmcmeekinb@mlb.com');
+insert into hotel_chain_email (id, hotel_chain_id, email) values (13, 5, 'bgippsc@ibm.com');
+insert into hotel_chain_email (id, hotel_chain_id, email) values (14, 2, 'mbellinod@squidoo.com');
+insert into hotel_chain_email (id, hotel_chain_id, email) values (15, 5, 'kmaylotte@adobe.com');
+insert into hotel_chain_email (id, hotel_chain_id, email) values (16, 3, 'ccarressf@booking.com');
+insert into hotel_chain_email (id, hotel_chain_id, email) values (17, 5, 'dcamillettig@odnoklassniki.ru');
+insert into hotel_chain_email (id, hotel_chain_id, email) values (18, 3, 'amckimmh@elegantthemes.com');
+insert into hotel_chain_email (id, hotel_chain_id, email) values (19, 5, 'aclemoni@seesaa.net');
+insert into hotel_chain_email (id, hotel_chain_id, email) values (20, 1, 'mprattenj@linkedin.com');
+insert into hotel_chain_email (id, hotel_chain_id, email) values (21, 4, 'czimekk@fda.gov');
+insert into hotel_chain_email (id, hotel_chain_id, email) values (22, 4, 'bclixbyl@geocities.jp');
+insert into hotel_chain_email (id, hotel_chain_id, email) values (23, 4, 'rhuguevillem@adobe.com');
+insert into hotel_chain_email (id, hotel_chain_id, email) values (24, 4, 'arustmann@google.fr');
+insert into hotel_chain_email (id, hotel_chain_id, email) values (25, 2, 'btalboyo@aboutads.info');
 
 -- ----------------------------
 -- Table structure for hotel_chain_phone
