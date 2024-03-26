bfb76cd5575319579a083e381dd25cfa6fef1af9
Colby Todd <cjhtodd11@gmail.com>
2024-03-25 22:36:39 -0400

    Adding extra sql files

---
+++ sql/hotel_chain.sql
@@ -0,0 +1,5 @@
+insert into hotel_chain (id, name, number_of_hotels, address_of_central_offices) values (1, 'Bogisich LLC', 8, '81396 Kings Crossing');
+insert into hotel_chain (id, name, number_of_hotels, address_of_central_offices) values (2, 'Kunde LLC', 8, '458 Clove Center');
+insert into hotel_chain (id, name, number_of_hotels, address_of_central_offices) values (3, 'Stamm, Bins and Hirthe', 8, '92044 Village Alley');
+insert into hotel_chain (id, name, number_of_hotels, address_of_central_offices) values (4, 'Vandervort, Leannon and Koelpin', 8, '5 Rowland Court');
+insert into hotel_chain (id, name, number_of_hotels, address_of_central_offices) values (5, 'Price, Schulist and Gusikowski', 8, '033 Kropf Circle');
