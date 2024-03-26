b0cbf7af2b8b8bbdccc18298da4ffd95500df184
Colby Todd <cjhtodd11@gmail.com>
2024-03-26 01:41:25 -0400

    Added indexes of room and rent table

--- sql/DB.sql
+++ sql/DB.sql
@@ -2451,3 +2451,17 @@ INNER JOIN hotel
 ON room.hotel_id = hotel.id
 GROUP BY hotel.id, hotel.name
 ORDER BY hotel.id;
+
+-- ----------------------------
+-- Index of Room Table
+-- ----------------------------
+DROP INDEX IF EXISTS room_index;
+CREATE INDEX room_index
+ON room (hotel_id, price, capacity, status);
+
+-- ----------------------------
+-- Index of Rent Table
+-- ----------------------------
+DROP INDEX IF EXISTS rent_index;
+CREATE INDEX rent_index
+ON rent (room_id, hotel_id, start_date, end_date);
