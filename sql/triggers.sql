2eaccc2729ddce754d80e4fc3e60b639a7c3d2fa
Colby Todd <cjhtodd11@gmail.com>
2024-03-27 15:57:32 -0400

    Added trigger to increment room count

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -10,7 +10,24 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE TRIGGER update_hotel_count
+CREATE OR REPLACE TRIGGER increment_hotel_count
 AFTER INSERT ON hotel
 FOR EACH ROW
 EXECUTE FUNCTION increment_hotel_count();
+
+-- ----------------------------
+-- Trigger to increment number_of_rooms
+-- ----------------------------
+CREATE OR REPLACE FUNCTION increment_room_count() RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE hotel
+        SET number_of_rooms = number_of_rooms + 1
+		WHERE hotel.id = NEW.hotel_id;
+			RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER increment_room_count
+AFTER INSERT ON room
+FOR EACH ROW
+EXECUTE FUNCTION increment_room_count();
