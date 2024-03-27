711d222fe02a885d8559a93098a9a732acd5e4dc
Colby Todd <cjhtodd11@gmail.com>
2024-03-27 16:02:10 -0400

    Added trigger to decrement hotel count

--- sql/DB.sql
+++ sql/DB.sql
@@ -2484,6 +2484,23 @@ FOR EACH ROW
 EXECUTE FUNCTION increment_hotel_count();
 
 -- ----------------------------
+-- Trigger to decrement number_of_hotels
+-- ----------------------------
+CREATE OR REPLACE FUNCTION decrement_hotel_count() RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE hotel_chain
+        SET number_of_hotels = number_of_hotels - 1
+		WHERE hotel_chain.id = OLD.hotel_chain_id;
+			RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER decrement_hotel_count
+AFTER DELETE ON hotel
+FOR EACH ROW
+EXECUTE FUNCTION decrement_hotel_count();
+
+-- ----------------------------
 -- Trigger to increment number_of_rooms
 -- ----------------------------
 CREATE OR REPLACE FUNCTION increment_room_count() RETURNS TRIGGER AS $$
